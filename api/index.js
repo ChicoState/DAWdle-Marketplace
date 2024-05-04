65c8b13c46af732d06f849cb975390ed1595f44a
Steven Lam <stevenl.inbox@gmail.com>
2024-05-04 09:43:52 -0700

    create post api route

--- api/index.js
+++ api/index.js
@@ -1,15 +1,21 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then( () => {console.log('MongoDB is connected')})
-.catch( (err) => {console.log(err)});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
@@ -17,19 +23,20 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(8000, () =>{
-    console.log('Server on port 8000');
-}) 
+app.listen(8000, () => {
+  console.log("Server on port 8000");
+});
 
-app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
-app.use((err,req,res,next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    });
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
