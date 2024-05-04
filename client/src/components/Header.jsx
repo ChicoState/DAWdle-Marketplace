8789ca2d469992776aecfa6e96c24f5e3ebf4a4f
Steven Lam <stevenl.inbox@gmail.com>
2024-05-04 08:24:59 -0700

    Finished header sign out

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,7 @@ import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
+import { signoutSuccess } from '../redux/user/userSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 
@@ -10,6 +11,22 @@ export default function Header() {
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <Navbar className="boarder-b-2">
@@ -55,12 +72,12 @@ export default function Header() {
               <span className="block text-sm font-medium truncate">
                 {currentUser.email}
               </span>
-            </Dropdown.Header>
-            <Link to={"/dashboard?tab=profile"}>
+              </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
-            <Dropdown.Item>Sign out</Dropdown.Item>
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
           </Dropdown>
         ) : (
           <Link to="/sign-in">
