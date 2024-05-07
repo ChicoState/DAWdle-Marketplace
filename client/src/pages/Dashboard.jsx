31db5bfcceb533bd205e0b964179e09a6584e73a
Steven Lam <stevenl.inbox@gmail.com>
2024-05-07 11:12:48 -0700

    Addings posts routes and finishing dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,26 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import {useLocation} from 'react-router-dom'
-import DashSidebar from '../components/DashSidebar';
-import DashProfile from '../components/DashProfile';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
 
 export default function Dashboard() {
   const location = useLocation();
-  const [tab, setTab] = useState('');
+  const [tab, setTab] = useState("");
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFromUrl = urlParams.get('tab');
+    const tabFromUrl = urlParams.get("tab");
     if (tabFromUrl) {
       setTab(tabFromUrl);
     }
   }, [location.search]);
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
-      <div className='md:w-56'>
+      <div className="md:w-56">
         {/*Sidebar*/}
-        <DashSidebar/>
+        <DashSidebar />
       </div>
       {/*Profile*/}
-      {tab === 'profile' && <DashProfile/>}
+      {tab === "profile" && <DashProfile />}
+      {tab === "posts" && <DashPosts />}
     </div>
-  )
+  );
 }
