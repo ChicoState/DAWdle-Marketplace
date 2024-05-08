81298fbeaf12827724ace33ca154b8b52998575c
Steven Lam <stevenl.inbox@gmail.com>
2024-05-07 23:52:14 -0700

    Add update post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import PostRoute from "./components/PostRoute";
 import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
 
 export default function App() {
   return (
@@ -26,6 +27,9 @@ export default function App() {
         </Route>
         <Route element={<PostRoute />}>
           <Route path="/create-post" element={<CreatePost />} />
+        </Route>
+        <Route element={<PostRoute />}>
+          <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
