7d28d942d8246d046e73d14a5fa920ee04c82bf8
Steven Lam <stevenl.inbox@gmail.com>
2024-05-08 10:53:30 -0700

    Modified code to for admin to see users

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -14,6 +14,6 @@ router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
-router.get("/getusers", verifyToken, getUsers);
+router.get("/getusers", getUsers);
 
 export default router;
