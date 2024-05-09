3e8debc6b58f7336f8eedb5c01d4121de4dd2248
Steven Lam <stevenl.inbox@gmail.com>
2024-05-09 00:19:06 -0700

    FInished about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            Welcome to DAWdle Marketplace
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>Quick Overview</p>
+
+            <p>
+              On this site, you will be able to browse and create posts for your
+              DAWdle adventures. You can interact with other users through posts
+              by commenting on their posts, liking them, and making your own.
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other users. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
